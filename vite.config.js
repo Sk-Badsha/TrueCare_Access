ed61e606e6d8d0efddcfd5c110902d9ee9c7b693
Sk Badsha <badshabhota@gmail.com>
2024-10-08 21:06:04 +0530

    Fix Backend Endpoint

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-dotenv.config();
-// https://vitejs.dev/config/
 
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": " https://truecare-access-backend.onrender.com",
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const config = {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_BACKEND_ENDPOINT,
+          changeOrigin: true,
+        },
+      },
     },
-  },
-  plugins: [react()],
-});
+  };
+  return defineConfig(config);
+};
